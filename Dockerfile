exo-docker/exo-chat-server

continous-release: update exo version to

Dockerfile
@@ -8,7 +8,7 @@
 # ----- build step 1
 FROM  exoplatform/jdk:8-ubuntu-1804 AS install
 
-ARG CHAT_SERVER_VERSION=3.1.0-M05
+ARG CHAT_SERVER_VERSION=
 
 COPY download.sh /
 RUN chmod u+x /download.sh && sync && /download.sh

exo-docker/exo

Dockerfile
@@ -34,7 +34,7 @@ RUN wget -nv -q -O /usr/bin/yq https://github.com/mikefarah/yq/releases/download
   } && chmod a+x /usr/bin/yq
 
 # Build Arguments and environment variables
-ARG EXO_VERSION=6.2.0-M27
+ARG EXO_VERSION=
 
 # this allow to specify an eXo Platform download url
 ARG DOWNLOAD_URL